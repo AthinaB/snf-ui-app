b7ff293c5f59be719530e418c98d406e30198df4
Athina Bekakou <athinabek@grnet.gr>
2016-11-15 18:13:13 +0200

    Add more loading messages

--- snf-ui/app/snf/routes/loading.js
+++ snf-ui/app/snf/routes/loading.js
@@ -6,11 +6,17 @@ export default Ember.Route.extend({
 
   setupController: function(controller, model) {
     var msg = [
-      'Go grab a coffee!', 
-      'You look cute today!',
+      'Go grab a coffee!',
+      'You look cute today...<br>You really do!',
       'What a beautiful day :)',
       'We\'ve missed you :)',
-      'Just wanted to say "Hi!"'
+      'Just wanted to say "Hi!"',
+      'Enjoy!',
+      'Come back soon!',
+      'Good to see you again!',
+      'It\'s never too late to start over :)',
+      'Good things<br>come to those who wait',
+      'How long would someone wait<br>to call it waiting?'
     ];
     controller.set('happy-msg', msg[_.random(0, msg.length - 1)]);
   }
